fcc0c40c0924564b6b9cf366cd6043862305f141
arun.prajapati <arunkp1122@gmail.com>
2026-07-31 00:48:47 +0545

    Add Cloudflare-compatible Sites build

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import vinext from "vinext";
+import { defineConfig } from "vite";
+import { sites } from "./build/sites-vite-plugin";
+
+export default defineConfig(async () => {
+  const { cloudflare } = await import("@cloudflare/vite-plugin");
+  return {
+    plugins: [vinext(), sites(), cloudflare({ viteEnvironment: { name: "rsc", childEnvironments: ["ssr"] } })],
+  };
+});
